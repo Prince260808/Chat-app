56020771fda607f4553803dabb05cfb252cba83c
Prince260808 <princewebdev8986@gmail.com>
2026-04-16 20:30:05 +0530

    fix

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: "https://chat-app-l9ba.onrender.com/api"
 });
 
 api.interceptors.request.use((config) => {
